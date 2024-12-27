aa9ca36a76cd8622c1001244ebee2fced78287ab
Flavio Roman <flavioromanweb@gmail.com>
2024-12-26 22:04:25 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './pages/products/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
